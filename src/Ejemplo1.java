72c433ef0767da8ac2bd10aea9d93797cb2a3288
ivan25cm <ivito1995@hotmail.com>
2015-11-06 17:59:00 +0100

    Con el run puesto

--- src/Ejemplo1.java
+++ src/Ejemplo1.java
@@ -1,4 +1,17 @@
+import acm.program.*;
+import acm.graphics.*;
+/*
+ * Autor Ivan barroso
+ * ejemplo github
+ * 
+ */
 
-public class Ejemplo1 {
 
+public class Ejemplo1 extends GraphicsProgram{
+
+	public void run (){
+		GLabel etiqueta =  new GLabel ("hola mundo");
+		add(etiqueta);
+		
+	}
 }
